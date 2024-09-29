f43094442e3b63fae38d5ff13f638c6bddf944ce
Ahmed7AI <163493601+Ahmed7AI@users.noreply.github.com>
2024-09-29 11:11:34 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-var form = document.getElementById('resume-form');
-var resumeDisplayElement = document.getElementById('resume-display');
-// Handle Form Submission
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var education = document.getElementById('education').value;
-    var experience = document.getElementById('experience').value;
-    var skills = document.getElementById('skills').value;
-    var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n   \n    <h3>Education</h3>\n    <p>").concat(experience, "</p>\n    \n    <h3>Skills</h3>\n    <p><b>Name:</b>").concat(name, "</p>\n    ");
-    if (resumeDisplayElement) {
-        resumeDisplayElement.innerHTML = resumeHTML;
+var toggleButton = document.getElementById('toggle-skills');
+var skills = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    // Toggling between 'block' and 'none' for the skills section
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
     }
     else {
-        console.error('The Resume display elemnet is missing');
+        skills.style.display = 'none';
     }
 });
--- script.ts
+++ script.ts
@@ -1,42 +1,13 @@
-const form = document.getElementById('resume-form') as HTMLFormElement
-const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement
+const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement;
+const skills = document.getElementById('skills') as HTMLElement;
 
-// Handle Form Submission
+toggleButton.addEventListener('click', () => {
+    // Toggling between 'block' and 'none' for the skills section
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    } else {
+        skills.style.display = 'none';
+    }
+});
 
-form.addEventListener('submit', (event: Event) => {
-    event.preventDefault();
-
-    const name = (document.getElementById('name') as HTMLInputElement).value
-    const email = (document.getElementById('email') as HTMLInputElement).value
-    const phone = (document.getElementById('phone') as HTMLInputElement).value
-    const education = (document.getElementById('education') as HTMLInputElement).value
-    const experience = (document.getElementById('experience') as HTMLInputElement).value
-    const skills = (document.getElementById('skills') as HTMLInputElement).value
-    
-    const resumeHTML = `
-    <h2><b>Editable Resume</b></h2>
-    <h3>Personal Information</h3>
-    <p><b>Name:</b><span contenteditable="true">${name}</span></p>
-    <p><b>Email:</b><span contenteditable="true">${email}</span></p>
-    <p><b>Phone:</b><span contenteditable="true">${phone}</span></p>
-
-
-    <h3>Education</h3>
-    <p contenteditable="true">${education}</p>
-   
-    <h3>Education</h3>
-    <p contenteditable="true">${experience}</p>
-    
-    <h3>Skills</h3>
-    <p contenteditable="true"><b>Name:</b>${name}</p>
-    `;
-
-
-if(resumeDisplayElement) {
-    resumeDisplayElement.innerHTML = resumeHTML;
-}else{
-    console.error('The Resume display elemnet is missing')
-
-}
-})
 
